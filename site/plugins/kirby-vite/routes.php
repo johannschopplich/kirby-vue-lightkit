94563ef073e0d50f252c6c9104d89c4625b99e78
Johann Schopplich <mail@johannschopplich.com>
2021-03-13 21:54:26 +0100

    refactor: throw error if controller isn't found

--- site/plugins/kirby-vite/routes.php
+++ site/plugins/kirby-vite/routes.php
@@ -1,29 +1,35 @@
 <?php
 
 use Kirby\Http\Response;
+use Kirby\Toolkit\Controller;
 
 return [
     [
         'pattern' => 'controllers/(:all).json',
-        'action' => function ($id) {
+        'action' => function ($name) {
             $kirby = kirby();
             $site = $kirby->site();
             $data = null;
 
             $cacheActive = env('KIRBY_CACHE', false) === true && $kirby->user() === null;
             $cacheBucket = $kirby->cache('pages');
-            $cacheKey = $id . '-json';
+            $cacheKey = $name . '-json';
 
-            if ($cacheActive) {
-                $data = $cacheBucket->get($cacheKey);
+            if ($cacheActive && $cacheBucket->exists($cacheKey)) {
+                return Response::json($cacheBucket->get($cacheKey));
             }
 
-            if ($data === null) {
-                $data = $kirby->controller($id, compact('kirby', 'site'));
+            $controller = Controller::load($kirby->root('controllers') . '/' . $name . '.php');
 
-                if ($cacheActive && !empty($data)) {
-                    $cacheBucket->set($cacheKey, $data);
-                }
+            if ($controller === null) {
+                return Response::json(['error' => 'Not Found'], 404);
+            }
+
+            $arguments = compact('kirby', 'site');
+            $data = $controller->call(null, $arguments);
+
+            if ($cacheActive) {
+                $cacheBucket->set($cacheKey, $data);
             }
 
             return Response::json($data);
