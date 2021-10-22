d8891ddc47fb3e13db274c86391246453fd130a2
Johann Schopplich <mail@johannschopplich.com>
2021-10-22 18:19:43 +0200

    chore: put Vite client types into env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,7 @@
+/// <reference types="vite/client" />
+/// <reference types="vite-plugin-pages/client" />
+/// <reference types="vite-plugin-vue-layouts/client" />
+
 declare module "*.vue" {
   import { DefineComponent } from "vue";
   // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
