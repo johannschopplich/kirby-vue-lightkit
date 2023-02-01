b0e60b37951f3994a387503b5b30bf837b1fec9e
Johann Schopplich <mail@johannschopplich.com>
2023-02-01 09:47:57 +0100

    fix: resolve page and site files

--- site/config/routes.php
+++ site/config/routes.php
@@ -1,5 +1,6 @@
 <?php
 
+use Kirby\Filesystem\F;
 use Kirby\Http\Response;
 use Kirby\Toolkit\Controller;
 
@@ -8,7 +9,8 @@ return [
         'pattern' => 'controllers/(:all).json',
         'action' => function ($key) {
             $kirby = kirby();
-            $data = $kirby->cache('pages')->getOrSet(
+            $cache = $kirby->cache('pages');
+            $data = $cache->getOrSet(
                 $key . '-json',
                 function () use ($kirby, $key) {
                     $controller = Controller::load($kirby->root('controllers') . '/' . $key . '.php');
@@ -31,9 +33,28 @@ return [
     ],
     [
         'pattern' => '(:all)',
-        'action' => function ($pageId) {
+        'action' => function ($path) {
             $kirby = kirby();
-            return $kirby->page($pageId) ?? $kirby->site()->homePage();
+            $extension = F::extension($path);
+
+            // Try to resolve page and site files
+            if (!empty($extension)) {
+                $id = dirname($path);
+                $filename = basename($path);
+
+                // Try to resolve image urls for pages and drafts
+                if ($page = $kirby->site()->findPageOrDraft($id)) {
+                    return $page->file($filename);
+                }
+
+                // Try to resolve site files at last
+                if ($file = $kirby->site()->file($filename)) {
+                    return $file;
+                }
+            }
+
+            // Resolve page by id or fall back to home page
+            return $kirby->page($path) ?? $kirby->site()->homePage();
         }
     ]
 ];
